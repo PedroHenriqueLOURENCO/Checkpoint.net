b3a5d15329d95844811f877f0b0b1639a770b1e7
pedrohenriquelourenco <rm98402@fiap.com.br>
2024-10-10 23:02:35 -0300

    subindo projeto cp net

---
+++ CP2.API/Controllers/FornecedorController.cs
@@ -0,0 +1,132 @@
+﻿using CP2.Application.Dtos;
+using CP2.Domain.Entities;
+using CP2.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace CP2.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FornecedorController : ControllerBase
+    {
+        private readonly IFornecedorApplicationService _applicationService;
+
+        public FornecedorController(IFornecedorApplicationService applicationService)
+        {
+            _applicationService = applicationService;
+        }
+
+        /// <summary>
+        /// get para fornecedores.
+        /// </summary>
+        /// <returns>Lista de fornecedores.</returns>
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<FornecedorEntity>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Get()
+        {
+            var objModel = _applicationService.ObterTodosFornecedores();
+
+            if (objModel is not null)
+                return Ok(objModel);
+
+            return BadRequest("erro obter os dados");
+        }
+
+        /// <summary>
+        /// get fornecedor by id
+        /// </summary>
+        /// <param name="id">ID do fornecedor.</param>
+        /// <returns>Fornecedor correspondente ao ID fornecido.</returns>
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(FornecedorEntity), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult GetPorId(int id)
+        {
+            var objModel = _applicationService.ObterFornecedorPorId(id);
+
+            if (objModel is not null)
+                return Ok(objModel);
+
+            return BadRequest("erro obter os dados");
+        }
+
+        /// <summary>
+        /// add um fornecedor
+        /// </summary>
+        /// <param name="entity">Dados do fornecedor a ser adicionado.</param>
+        /// <returns>Fornecedor adicionado.</returns>
+        [HttpPost]
+        [ProducesResponseType(typeof(FornecedorEntity), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Post([FromBody] FornecedorDto entity)
+        {
+            try
+            {
+                var objModel = _applicationService.SalvarDadosFornecedor(entity);
+
+                if (objModel is not null)
+                    return Ok(objModel);
+
+                return BadRequest("erro ao  salvar os dados");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new
+                {
+                    Error = ex.Message,
+                    status = HttpStatusCode.BadRequest,
+                });
+            }
+        }
+
+        /// <summary>
+        /// patch fornecedores (atualizar)
+        /// </summary>
+        /// <param name="id">ID do fornecedor a ser atualizado.</param>
+        /// <param name="entity">Novos dados do fornecedor.</param>
+        /// <returns>Fornecedor atualizado.</returns>
+        [HttpPut("{id}")]
+        [ProducesResponseType(typeof(FornecedorEntity), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Put(int id, [FromBody] FornecedorDto entity)
+        {
+            try
+            {
+                var objModel = _applicationService.EditarDadosFornecedor(id, entity);
+
+                if (objModel is not null)
+                    return Ok(objModel);
+
+                return BadRequest("erro ao salvar os dados");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new
+                {
+                    Error = ex.Message,
+                    status = HttpStatusCode.BadRequest,
+                });
+            }
+        }
+
+        /// <summary>
+        /// delete fornecedor by id
+        /// </summary>
+        /// <param name="id">ID do fornecedor a ser deletado.</param>
+        /// <returns>Fornecedor deletado.</returns>
+        [HttpDelete("{id}")]
+        [ProducesResponseType(typeof(FornecedorEntity), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Delete(int id)
+        {
+            var objModel = _applicationService.DeletarDadosFornecedor(id);
+
+            if (objModel is not null)
+                return Ok(objModel);
+
+            return BadRequest("erro ao deletar os dados");
+        }
+    }
+}
---
+++ CP2.Application/Dtos/VendedorDto.cs
@@ -0,0 +1,37 @@
+﻿using CP2.Domain.Interfaces.Dtos;
+using FluentValidation;
+using System;
+
+namespace CP2.Application.Dtos
+{
+    public class VendedorDto : IVendedorDto
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Endereco { get; set; }
+        public DateTime DataContratacao { get; set; }
+        public decimal ComissaoPercentual { get; set; }
+        public decimal MetaMensal { get; set; }
+        public DateTime CriadoEm { get; set; }
+
+        public void Validate()
+        {
+            var validateResult = new VendedorDtoValidation().Validate(this);
+
+            if (!validateResult.IsValid)
+                throw new Exception(string.Join(" erre ", validateResult.Errors.Select(x => x.ErrorMessage)));
+        }
+    }
+
+    internal class VendedorDtoValidation : AbstractValidator<VendedorDto>
+    {
+        public VendedorDtoValidation()
+        {
+            RuleFor(x => x.Nome).NotEmpty().WithMessage("nome deve ser valido");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("precisa inserir email valido");
+            RuleFor(x => x.ComissaoPercentual).InclusiveBetween(0, 100).WithMessage("Comissão é até 100 %.");	
+        }
+    }
+}
